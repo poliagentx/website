a0c872ac2091c9604040a485688cc95872c09301
SCT211-0427/2023 GEORGE OTIENO <148488211+NGIYEG@users.noreply.github.com>
2025-07-22 19:04:11 +0300

    theme svgs change in mcedium screens and below

---
+++ Assets/Js/humming.js
@@ -0,0 +1,13 @@
+
+     
+        (function() {
+            const savedTheme = localStorage.getItem('theme');
+            const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+            if (savedTheme === 'dark' || (!savedTheme && prefersDark)) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
+        })();
+    
